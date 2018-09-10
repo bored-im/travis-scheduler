1e6b7447955467604fdda6dac410f6979cfe207a
Yuva <yuva@codemancers.com>
2018-09-10 18:28:09 +0530

    change env for redis url

--- lib/travis/scheduler/config.rb
+++ lib/travis/scheduler/config.rb
@@ -18,7 +18,7 @@ module Travis
              plans:      { },
              queue:      { default: 'builds.gce', redirect: {} },
              queues:     [ queue: 'name', os: 'os', dist: 'dist', group: 'group', sudo: false, osx_image: 'osx_image', language: 'language', owner: 'owner', slug: 'slug', services: ['service']],
-             redis:      { url: 'redis://localhost:6379' },
+             redis:      { url: ENV["TRAVIS_SCHEDULER_REDIS_URL"] },
              sentry:     { },
              sidekiq:    { namespace: 'sidekiq', pool_size: 3, log_level: :warn },
              ping:       { interval: 5 * 60 },
